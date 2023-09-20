f2510eeaf08e95ac319451b49fe88b2f96226bfc
Kajsa Møllersen <kajsa.mollersen@uit.no>
2023-09-20 13:47:39 +0200

    looking for theta_SOTA

--- Parameters_PublicCompetition.R
+++ Parameters_PublicCompetition.R
@@ -12,7 +12,9 @@ theta_max = theta
 step = (theta_max-theta_min)/(m-1)
 theta_vec = seq(theta_min, theta_max, step)
 
-theta_trunc = sqrt(0.9225) # truncating for the bootstrap
+# theta_trunc = sqrt(0.9225) # truncating for the bootstrap. a bit too high, try again
+# theta_trunc = sqrt(0.917) # what is this sqrt?
+theta_trunc = 0.9255
 
 rho = 0.6 # correlation coefficient, the number is calculated from Mania (2019)
 
