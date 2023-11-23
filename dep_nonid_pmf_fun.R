adfa43849536ddeab2061e60fca833b4213af6f3
Kajsa Møllersen <kajsa.mollersen@uit.no>
2023-11-23 10:28:55 +0100

    typo, need to check, R altsaa

--- dep_nonid_pmf_fun.R
+++ dep_nonid_pmf_fun.R
@@ -6,13 +6,14 @@ dep_nonid_pmf <- function(n, m, rho, rep, theta_vec, theta_0){
   # correlation rho = corr(Y_0, Y_j). The m classifiers are independent of each other given Y_0.
   
   sigma_0 = theta_0*(1-theta_0)
-  sigma_vec = sigma_vec = theta_vec*(1-theta_vec)
+  sigma_vec = theta_vec*(1-theta_vec)
   
   p_flip1 = (theta_0 - rho*sqrt(sigma_0*sigma_vec) - theta_0*theta_vec)/theta_0 # P(Y_j = 0|Y_0 = 1)
   p_flip0 = (-rho*sqrt(sigma_0*sigma_vec)+(1-theta_0)*theta_vec)/(1-theta_0) # P(Y_j = 1|Y_0 = 0)
   
   min_dep = numeric(rep) # min number of failures with dependency
   min_indep = numeric(rep) # for independent, as a check
+  teamsSOTA = numeric(rep) # for independent, as a check
   
   for (ell in 1:rep){
     
@@ -25,11 +26,13 @@ dep_nonid_pmf <- function(n, m, rho, rep, theta_vec, theta_0){
     flip0 = rbinom(m,n-theta_y0,p_flip0) # flipping incorrect predictions
     
     x_dep = n-(theta_y0-flip1+flip0) # number of wrong predictions for each classifier
+    hat_theta = (n-x_dep)/n
+    teamsSOTA[ell] = length(hat_theta[hat_theta > theta_0])
     
     min_dep[ell] = min(x_dep)
   }
   
-  X = list(min_dep = min_dep, x_dep = x_dep)
+  X = list(min_dep = min_dep, x_dep = x_dep, teamsSOTA = teamsSOTA)
   
   return(X)
 }
