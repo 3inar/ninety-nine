8db8b4acb785921109278ac89f4ac263f4902674
Kajsa Møllersen <kajsa.mollersen@uit.no>
2024-08-30 15:36:20 +0200

    better figures

--- dependent_nonidentical.R
+++ dependent_nonidentical.R
@@ -88,3 +88,61 @@ Vsota = mean(X$min_fail*X$min_fail) - Esota*Esota
 sprintf("The simulated expected value is %.7f and a standard deviation is %.7f, with %s repetitions.",  
         (n-Esota)/n, sqrt(Vsota)/n, rep)
 
+###########################################################################
+### Expected value/bias/variance as a function of rho ################
+###########################################################################
+
+# theta_min can be only so small
+trunc_min = ((rho*rho)*theta_0/(1-theta_0))/(1+(rho*rho)*theta_0/(1-theta_0))
+
+rho_vec = seq(0.0, 1.0, by=0.01)
+
+ylm = c(0.0,0.02)
+
+theta_min_vec = seq(0.8,theta, by=0.025)
+
+
+Esota_theta_vec = matrix(0,length(theta_min_vec),length(rho_vec))
+
+
+for (i in 1:length(theta_min_vec)){
+  theta_min = theta_min_vec[i] # for the non-identical
+  theta_max = theta 
+  step = (theta_max-theta_min)/(m-1)
+  theta_vec = seq(theta_min, theta_max, step)
+
+  for (j in 1:length(rho_vec)){
+    rho = rho_vec[j] # for the correlated
+  
+    X = dep_nonid_pmf(n, m, rho, rep, theta_vec, theta_0 = theta)
+  
+    # Expected value
+    Esota = mean(X$min_fail)
+    Esota_theta_vec[i,j] = (1-Esota/n)-theta
+    print(c(i,j))
+  }
+}
+
+# plotting for theta_min = 0.875
+plot(rho_vec, Esota_theta_vec[4,],"l", lty = 1, col = "darkgreen", ylim = ylm,
+     xlab = "", ylab = "")
+par(new=TRUE) # new plot in same window
+
+for (i in 1:3){ 
+  plot(rho_vec, Esota_theta_vec[i,],"l", lty = i+2, col = "darkgreen", ylim = ylm,
+       xlab = "", ylab = "")
+  par(new=TRUE) # new plot in same window
+}
+
+# plotting for theta_min = theta
+plot(rho_vec, Esota_theta_vec[5,],"l", lty = 1, col = "red", ylim = ylm,
+     main = TeX(r'(Bias as a function of correlation, non-identical ${theta}$s)'), xlab = TeX(r'(${rho}_0$)'), ylab = TeX(r'($E \hat{theta}_{SOTA} - {theta}_{SOTA}$)'))
+par(new=TRUE) # new plot in same window
+
+legend(0.7, 0.02, legend=c(TeX(r'(${theta}_{min}=0.9$)'),TeX(r'(${theta}_{min}=0.875$)'), TeX(r'(${theta}_{min}=0.850$)'), 
+                           TeX(r'(${theta}_{min}=0.825$)'), TeX(r'(${theta}_{min}=0.800$)')),
+       col=c("red","darkgreen","darkgreen","darkgreen", "darkgreen"), lty=c(1,1,5,4,3), cex=0.8)
+
+abline(v=0.6, col="gray")
+
+
