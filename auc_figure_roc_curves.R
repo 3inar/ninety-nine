4f048edfbb9df632e39853a253ad837de8fc9b06
einar <einar.j.holsbo@uit.no>
2024-10-30 10:01:22 +0100

    update this figure

--- auc_figure_roc_curves.R
+++ auc_figure_roc_curves.R
@@ -14,6 +14,7 @@ clasr <- make_classifier(ex_auc)
 ms <- mus(clasr)
 ss <- sds(clasr)
 
+set.seed(2024-05-24)
 png("auc_figure_rocB.png", width=6, height=4, units="in", res=300)
 curve(dnorm(x, ms[1], ss[1]), xlim=c(-5+ms[2], 5), col="black", lwd=2,
         main="Density of S- and S+ for AUC = .9",
@@ -21,21 +22,42 @@ curve(dnorm(x, ms[1], ss[1]), xlim=c(-5+ms[2], 5), col="black", lwd=2,
         ylab="Density")
 curve(dnorm(x, ms[2], ss[2]), add=T, col="grey", lwd=2)
 
-set.seed(2024-05-24)
 rg_f <- rnorm(nfa, ms[2], ss[2])
 rg_t <- rnorm(ntr, ms[1], ss[2])
 rug(rg_f, col="grey", lwd=1.5)
 rug(rg_t, col="black", lwd=1.5)
 dev.off()
 
+set.seed(2024-10-24)
 png("auc_figure_rocA.png", width=6, height=4, units="in", res=300)
-preds <- predict(clasr, ntr, nfa)
-roccc <- roc(response=preds$truth, predictor=preds$predicted)
-plot(roccc, lwd=1, main="ROC curves for 100 simulated competitions, AUC = .9")
+  # simulate 1000 competitors
+  lst <- list()
+  for (i in 1:1000) {
+          preds <- predict(clasr, ntr, nfa)
+          lst[[length(lst)+1]] <- roc(response=preds$truth, predictor=preds$predicted)
+  }
 
-for (i in 1:99) {
-        preds <- predict(clasr, ntr, nfa)
-        roccc <- roc(response=preds$truth, predictor=preds$predicted)
-        plot(roccc, lwd=1, add=T)
-}
+  # what are their observed aucs? what are the inner quartiles and max of these>
+  acs <- sapply(lst, auc)
+  inner_quart <- quantile(acs, probs=c(1/4, 3/4))
+  mxx <- which.max(acs)
+
+  # pull out the maximum curve
+  maxline <- lst[[mxx]]
+  lst[[mxx]] <- NULL
+  acs <- acs[-mxx]
+
+  # which curves belong to the inner quartile region?
+  inner <- acs > inner_quart[1] & acs < inner_quart[2]
+
+  # plot the middle curves in dark grey, the outer ones in light grey, the max auc one in black
+  plot(lst[[1]], type="n")
+  for (roccc in lst[!inner]) {
+    plot(roccc, lwd=1, add=T, col="#dddddd")
+  }
+  for (roccc in lst[inner]) {
+    plot(roccc, lwd=1, add=T, col="#999999")
+  }
+
+  plot(maxline, lwd=2, add=T, col="black")
 dev.off()
